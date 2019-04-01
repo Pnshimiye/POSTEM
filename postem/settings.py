6d7ebd3b9693b8d7559e0bc2d1da7c116931ecef
Pnshimiye <pnshimiye@gmail.com>
2019-04-01 10:44:41 +0200

    create database

--- postem/settings.py
+++ postem/settings.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'postem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postem',
+        'USER': 'pauline',
+    'PASSWORD':'kalioli050718',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Kigali'
 
 USE_I18N = True
 
