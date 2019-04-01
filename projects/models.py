270f323c6558ea24ecae70e2f2f73866125e195c
Pnshimiye <pnshimiye@gmail.com>
2019-04-01 10:25:53 +0200

    add templates for registration

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.contrib.auth.models import User  
+from tinymce.models import HTMLField
 
-# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length = 50)
+    caption = models.CharField(max_length=100)     
+    profile = models.ForeignKey(User,on_delete=models.CASCADE)  
+    
+    upload_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='profile')
+    likes = models.BooleanField(default=False)
+   
+
+    class Meta:
+        ordering = ('upload_date',)
+
+    def save_image(self):
+        self.save()
+    
+    @classmethod
+    def update_caption(cls, update):
+        pass
+    
+    @classmethod
+    def get_image_id(cls, id):
+        image = Image.objects.get(pk=id)
+        return image
+    
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk = profile)
+        return images
+    
+    @classmethod
+    def get_all_images(cls):
+        images = Image.objects.all()
+        return images
+
+
+class Profile(models.Model):
+    prof_pic = models.ImageField(upload_to='profile/',blank=True)
+    bio = HTMLField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+
+
+    def save_profile(self):
+        self.save()
+    
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+    
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+
+ 
+
+
+class Comments(models.Model):
+    profile=models.ForeignKey(Profile,null=True)
+    comment = HTMLField()     
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.user_name
+
+        
+    def save_comment(self):
+        self.save()
+    
+    @classmethod
+    def get_comments_by_images(cls, id):
+        comments = Comments.objects.filter(image__pk = id)
+        return comments
---
+++ projects/urls.py
@@ -0,0 +1,17 @@
+
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    url(r'^$',views.home,name='home'),  
+    url(r'^upload/$', views.upload_image, name='upload_image'),
+    url(r'^accounts/edit',views.edit_profile, name='edit_profile'),
+    url(r'^profile', views.profile, name='profile'), 
+    url(r'^comments/(\d+)',views.comments,name="comments")    
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
