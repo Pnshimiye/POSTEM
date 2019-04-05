97952d1a3feb82cedc9899cb2c9d527062ccf39e
Pnshimiye <pnshimiye@gmail.com>
2019-04-05 18:52:32 +0200

    add a text for Project

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+ 
+class ProjectTestCase(TestCase):
+    # Set Up Method
+    def setUp(self):
+        self.Instagram = name(category_name='Food') ad
+        self.food.save_category()
+
+        self.nairobi = Location(location_name='Kigali')
+        self.nairobi.save_location()
+
+        self.image = Image(name='Sea food',description="Sea food form Sakae a Japanese restaurant", location = self.location, category= self.category)
+        self.image.save_image()
+    
